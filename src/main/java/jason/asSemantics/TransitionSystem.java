9e2b30dc6ac6c519c221584c1d7fbb0f52088b3e
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-05-04 22:36:07 -0300

    Update TransitionSystem.java

--- src/main/java/jason/asSemantics/TransitionSystem.java
+++ src/main/java/jason/asSemantics/TransitionSystem.java
@@ -146,10 +146,12 @@ public class TransitionSystem implements Serializable {
 
         //2. add entry to the map of critical plans
          planL0.add(cp0);
+         C.CPM.add(cp0);
          C.CRL.add(pb0);
          //C.getCLM().put(tp0.getPredicateIndicator(), planL0);
 
          planL1.add(cp1);
+         C.CPM.add(cp1);
          C.CRL.add(pb1);
          //C.getCLM().put(tp1.getPredicateIndicator(), planL1);
 
