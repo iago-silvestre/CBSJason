e0a892767772cd927405c1f19dfff376379b830a
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-06-22 14:08:15 -0300

    Update TransitionSystem.java

--- src/main/java/jason/asSemantics/TransitionSystem.java
+++ src/main/java/jason/asSemantics/TransitionSystem.java
@@ -1844,7 +1844,8 @@ public class TransitionSystem implements Serializable {
                                     // Print the matched unifier result for now
                                     // You can store or use this however needed
                                     getLogger().info("[Query] Matched " + queryLit + " with " + belief + " => " + uq);
-                                    u.compose(uq);  // <-- This is what binds FRL to 5 for the rest of the plan
+                                    im.unif = im.unif.compose(uq); 
+                                    //u.compose(uq);  // <-- This is what binds FRL to 5 for the rest of the plan
                                     break; // you probably want just the first match
                                 }
                             }
