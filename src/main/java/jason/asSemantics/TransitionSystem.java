f1dcd368cf2de61db5f58de42d8489028fdb66e8
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-06-29 23:21:50 -0300

    Update TransitionSystem.java

--- src/main/java/jason/asSemantics/TransitionSystem.java
+++ src/main/java/jason/asSemantics/TransitionSystem.java
@@ -1802,8 +1802,12 @@ public class TransitionSystem implements Serializable {
 
                     Literal body = null;
                     //IntendedMeans im = curInt.peek();
-                    IntendedMeans im = curInt.peek();
-                    Unifier     u = im.unif;
+                    try {
+                    IntendedMeans im = curInt.peek();  // May throw NullPointerException
+                    Unifier u = im.unif;                        // May also throw if im is null
+                    } catch (Exception e) {
+                    Unifier u = new Unifier();                 // Fallback: create new Unifier
+                    }
             
                     if (bTerm instanceof Literal)
                         bodyTer = (Literal)bTerm; 
