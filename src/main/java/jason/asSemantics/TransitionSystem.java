28b151e0f3ddb2bc5fd777e3d0ab4c6e7a076ef6
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-08-03 18:42:04 -0300

    Update TransitionSystem.java

--- src/main/java/jason/asSemantics/TransitionSystem.java
+++ src/main/java/jason/asSemantics/TransitionSystem.java
@@ -1800,7 +1800,7 @@ public class TransitionSystem implements Serializable {
                     final Intention curInt = C.SI;
                     //joinRenamedVarsIntoIntentionUnifier(curInt.peek(), curInt.peek().unif); //test if renamedvars are missing into context
 
-                    Literal body = null;
+                    /*Literal body = null;   //Going back to old version for now to debug
                     //IntendedMeans im = curInt.peek();
                     Unifier u;
                     try {
@@ -1808,7 +1808,10 @@ public class TransitionSystem implements Serializable {
                     u = im.unif;                        // May also throw if im is null
                     } catch (Exception e) {
                     u = new Unifier();                 // Fallback: create new Unifier
-                    }
+                    }*/
+                    Literal body = null;
+                    IntendedMeans im = curInt.peek();
+                    Unifier     u = im.unif;
             
                     if (bTerm instanceof Literal)
                         bodyTer = (Literal)bTerm; 
@@ -1992,7 +1995,7 @@ public class TransitionSystem implements Serializable {
                         //System.out.println("TS expedited-deliberate | context = " + context);
 
                         // int i=0;
-                        if (r != null && r.hasNext()){
+                        if (r != null && r.hasNext()){       //Multiple Lines plans
                             //plan.setUnifier(r.next());
                             //System.out.println("  [debug] r != null && r.hasNext()" );
                             PlanBody current = plan.getBody();
