3e37c5f5936ddb0b9e46dcaba949ebb866527558
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-08-03 22:50:38 -0300

    Update TransitionSystem.java

--- src/main/java/jason/asSemantics/TransitionSystem.java
+++ src/main/java/jason/asSemantics/TransitionSystem.java
@@ -1800,7 +1800,7 @@ public class TransitionSystem implements Serializable {
                     final Intention curInt = C.SI;
                     //joinRenamedVarsIntoIntentionUnifier(curInt.peek(), curInt.peek().unif); //test if renamedvars are missing into context
 
-                    /*Literal body = null;   //Going back to old version for now to debug
+                    Literal body = null;   //Going back to old version for now to debug
                     //IntendedMeans im = curInt.peek();
                     Unifier u;
                     try {
@@ -1808,10 +1808,7 @@ public class TransitionSystem implements Serializable {
                     u = im.unif;                        // May also throw if im is null
                     } catch (Exception e) {
                     u = new Unifier();                 // Fallback: create new Unifier
-                    }*/
-                    Literal body = null;
-                    IntendedMeans im = curInt.peek();
-                    Unifier     u = im.unif;
+                    }
             
                     if (bTerm instanceof Literal)
                         bodyTer = (Literal)bTerm; 
