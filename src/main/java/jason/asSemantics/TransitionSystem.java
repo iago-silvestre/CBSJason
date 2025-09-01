b66928bfec643c77d4c86f2f1ff3c23c75834eea
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-09-01 00:26:41 -0300

    Update TransitionSystem.java

--- src/main/java/jason/asSemantics/TransitionSystem.java
+++ src/main/java/jason/asSemantics/TransitionSystem.java
@@ -1966,7 +1966,7 @@ public class TransitionSystem implements Serializable {
             //if (entry.getValue()) { \\No need, it only has currently-valid CPs
                 //PredicateIndicator  cpKey = entry.getKey();
                 //System.out.println("cpKey = " + cpKey);
-                PredicateIndicator cpKey = "+cb0/0";
+                PredicateIndicator cpKey = +cb0/0;
                 /*for (Literal bel : ag.getBB()) {
                     if (bel.getFunctor().equals("critical_percept") && bel.getArity() == 1) {
                         System.out.println("  [debug] critical_percept value: " + bel.getTerm(0));
